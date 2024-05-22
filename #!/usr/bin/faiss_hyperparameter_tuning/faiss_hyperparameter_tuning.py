2dc5cb0b5985ca741143960a24c98d48472c0326
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2024-05-22 12:15:57 -0700

    Create faiss_hyperparameter_tuning.py

---
+++ #!/usr/bin/faiss_hyperparameter_tuning/faiss_hyperparameter_tuning.py
@@ -0,0 +1,20 @@
+
+cache_dir = r"C:\Users\Soli1\.cache"
+model = SentenceTransformer("all-MiniLM-L6-v2", cache_folder=cache_dir)
+faiss_title_embedding = model.encode(pdf_subset.title.values.tolist())
+
+
+pdf_to_index = pdf_subset.set_index(["id"], drop=False)
+id_index = np.array(pdf_to_index.id.values).flatten().astype("int")
+content_encoded_normalized = faiss_title_embedding.copy()
+faiss.normalize_L2(content_encoded_normalized)
+
+index_content = faiss.IndexIDMap(faiss.IndexFlatIP(len(faiss_title_embedding[0])))
+index_content.add_with_ids(content_encoded_normalized, id_index)
+
+param_grid = {
+    'pretrained_model': ["all-MiniLM-L6-v2", "roberta-base", "bert-base-uncased"],
+    'index_dimension': [256, 512, 1024],
+    'similarity_threshold': [0.7, 0.8, 0.9]
+}
+

bfccb370ab2dbff401aaecfc45b78bbff8795885
Solmaz Seyed Monir <55103176+solmazsm@users.noreply.github.com>
2025-05-05 00:32:08 -0700

    Update coarse_fine_multivector.cpp

--- cppCode/VS/src/coarse_fine_multivector.cpp
+++ cppCode/VS/src/coarse_fine_multivector.cpp
@@ -5,59 +5,53 @@
 #include <string>
 #include <fstream>
 
-
 void runMultiVectorRetrieval(const Preprocess& prep, int k, const std::vector<int>& query_ids, const std::string& result_file_path) {
-    std::cout << "\n[Coarse-Fine Multi-Vector Retrieval] Starting...\n";
+    std::cout << "\n[VectorSearch: Coarse-Fine Multi-Vector Retrieval] Starting...\n";
 
     std::vector<std::vector<float>> multi_query;
     for (int qid : query_ids) {
         multi_query.emplace_back(prep.data.query[qid], prep.data.query[qid] + prep.data.dim);
     }
 
-    
-    std::vector<std::pair<float, int>> score_all(prep.data.N);
-
+    std::vector<std::pair<float, int>> coarse_scores(prep.data.N);
     for (int i = 0; i < prep.data.N; ++i) {
         float total_dist = 0.0f;
         for (const auto& qvec : multi_query) {
             total_dist += cal_distSqrt(qvec.data(), prep.data.val[i], prep.data.dim);
         }
-        score_all[i] = { total_dist / multi_query.size(), i };
+        coarse_scores[i] = { total_dist / multi_query.size(), i };
     }
 
-    std::sort(score_all.begin(), score_all.end());
+    std::sort(coarse_scores.begin(), coarse_scores.end());
 
- 
-    std::vector<int> top_candidates;
+    std::vector<int> coarse_top_k;
     int coarse_k = 100;
-    for (int i = 0; i < std::min(coarse_k, (int)score_all.size()); ++i) {
-        top_candidates.push_back(score_all[i].second);
+    for (int i = 0; i < std::min(coarse_k, (int)coarse_scores.size()); ++i) {
+        coarse_top_k.push_back(coarse_scores[i].second);
     }
 
-   
-    CoarseIndexer coarse_index(prep.data.dim);
-    FineReranker fine_index(prep.data.dim, 32); // M = 32
-    fine_index.hnsw.efSearch = 64;
+    VectorSearchCoarseIndexer coarse_indexer(prep.data.dim);
+    VectorSearchRefiner refiner(prep.data.dim, 32);  // M = 32
+    refiner.hnsw.efSearch = 64;
 
-    for (int id : top_candidates) {
-        fine_index.add(prep.data.val[id]); // add top coarse results
+    for (int id : coarse_top_k) {
+        refiner.add(prep.data.val[id]);  // add selected coarse candidates
     }
 
-  
-    std::vector<std::pair<float, int>> reranked;
+    std::vector<std::pair<float, int>> refined_scores;
     for (int i = 0; i < prep.data.N; ++i) {
         float total_dist = 0.0f;
         for (const auto& qvec : multi_query) {
             total_dist += cal_distSqrt(qvec.data(), prep.data.val[i], prep.data.dim);
         }
-        reranked.push_back({ total_dist / multi_query.size(), i });
+        refined_scores.push_back({ total_dist / multi_query.size(), i });
     }
 
-    std::sort(reranked.begin(), reranked.end());
+    std::sort(refined_scores.begin(), refined_scores.end());
 
     std::ofstream fout(result_file_path);
     for (int i = 0; i < k; ++i) {
-        fout << query_ids[0] << " " << reranked[i].second << "\n";
+        fout << query_ids[0] << " " << refined_scores[i].second << "\n";
     }
     fout.close();
 

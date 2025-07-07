d867ca1f66024383e38448219ffa2c1bfb18297a
Solmaz Seyed Monir <solmazsm@uw.edu>
2025-07-07 14:04:00 -0700

    Delete Vectorse/python directory

--- Vectorse/python/mkdir cache/cache_dir_soli.py
+++
@@ -1,7 +0,0 @@
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2", 
-    cache_folder=cache_dir
-)
-
-faiss_title_embedding = model.encode(pdf_subset.title.values.tolist())
-len(faiss_title_embedding), len(faiss_title_embedding[0])
